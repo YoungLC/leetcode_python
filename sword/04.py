876ae0f08e7cc18c0f0531399f81ae55487ec2c3
liuyangyang <liuyangyang@chinamobile.com>
2021-01-21 18:52:21 +0800

    a:wq

---
+++ sword/04.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findNumberIn2DArray(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or len(matrix)==0 or len(matrix[0])== 0:
+            return False
+
